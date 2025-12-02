07d1548ab8c6cd1fc9a7c7d84bdc08abad64eaf4
Ніколаєв <danylo.nikolaiev@kgemt.org.ua>
2025-12-02 09:43:00 +0200

    Improve welcome message with greeting

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -1,8 +1,9 @@
 public class MessagePrinter {
     
     public static String getWelcomeMessage() {
-        return "Welcome to Git and GitHub!";
+        return "Hello! Welcome to Git and GitHub Tutorial!";
     }
+
     
     public static String getDateMessage() {
         return "Today is: " + java.time.LocalDate.now();
@@ -11,6 +12,7 @@ public class MessagePrinter {
     public static String getGoodbyeMessage() {
         return "Thank you for using MessagePrinter!";
     }
+    
 
     
     public static void main(String[] args) {
